177cb742d556f177e391e3fab0ecab8b52859cef
unknown <drakewang@gmail.com>
2022-10-04 23:12:29 -0700

    Add walkthrough

--- src/Home.js
+++ src/Home.js
@@ -6,8 +6,20 @@ export class Home extends React.Component {
 	}
 	render() {
 		return (
-				<div className="text-center">
-					<h4>Welcome</h4>
+				<div className="text-center mt-4">
+					<h4>Site Walkthrough</h4>
+
+					<div className="youtube-video mt-4">
+						<iframe 
+							width="560" 
+							height="315" 
+							src="https://www.youtube.com/embed/79C_6_cPe2A" 
+							title="YouTube video player" 
+							frameborder="0" 
+							allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+							allowfullscreen>
+						</iframe>
+					</div>
 				</div>
 		)
 	}
