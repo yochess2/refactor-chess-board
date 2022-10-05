a759b747dc6654ae16b68834f3339962b4d95625
unknown <drakewang@gmail.com>
2022-10-05 01:03:05 -0700

    Add repo link

--- src/Home.js
+++ src/Home.js
@@ -5,23 +5,26 @@ export class Home extends React.Component {
 		document.title = "YoChess"
 	}
 	render() {
-		return (
-				<div className="text-center mt-4">
-					<h4>Site Walkthrough</h4>
+		return (<>
+			<div className="text-center mt-4">
+				<h4>Site Walkthrough</h4>
 
-					<div className="youtube-video mt-4">
-						<iframe 
-							width="560" 
-							height="315" 
-							src="https://www.youtube.com/embed/79C_6_cPe2A" 
-							title="YouTube video player" 
-							frameborder="0" 
-							allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
-							allowfullscreen>
-						</iframe>
-					</div>
+				<div className="youtube-video mt-4">
+					<iframe 
+						width="560" 
+						height="315" 
+						src="https://www.youtube.com/embed/79C_6_cPe2A" 
+						title="YouTube video player" 
+						frameborder="0" 
+						allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+						allowfullscreen>
+					</iframe>
 				</div>
-		)
+				<div>
+					<p>Link to Repo is on <a target="_blank" href="https://github.com/yochess2/refactor-chess-board">Github</a></p>
+				</div>
+			</div>
+	</>)
 	}
 }
 
